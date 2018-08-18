ac8c7a44cb1dd999f7b27bc921cc60f110aec696
RefiPeretz <refi11991@gmail.com>
2018-08-18 19:11:52 +0300

    Add Courses comp

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import {Link} from 'react-router'
+import React from 'react';
+import { Link } from 'react-router';
 
-class HomePage extends React.Component{
-    render(){
-        return(<div>className="jumpbutton">
-                    <h1>Pluralsight Admin</h1>
-                    <p>React ,Reduc and readt bla bla</p>
-                    <Link to="about" className="btn btn-primary btn-lg">Learn More</Link>
-                    </div>);
+class HomePage extends React.Component {
+    render() {
+        return (
+            <div className="jumbotron">
+                <h1>Pluralsight Admin</h1>
+                <p>React, Redux and React Router in ES6 for ultra-responsive web apps.</p>
+                <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
+            </div>
+        );
     }
 }
 
